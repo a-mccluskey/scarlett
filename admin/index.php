1d250ba5f43e1a4c3a119eff34e2658808f28141
a-mccluskey <46331308+a-mccluskey@users.noreply.github.com>
2020-05-09 18:27:04 +0100

    revamp admin/index

--- admin/index.php
+++ admin/index.php
@@ -17,39 +17,47 @@ include('session.php');
 echo "Welcome " . $_SESSION['friendlyName']. "<br>";
 ?>
 Useful Features that will need linking to...<br>
-Also some description. Ideally should be a lighter weight than the content pages, less images etc. perhaps needing a separate style sheet?<br></P>
-<h2>Promo banner manager</h2>
-<p>Image and links for the promotion banner at the top of the home page.</p>
-<p>
-<a href="promo_adder.php">Add item to promo banner</a><br>
-<a href="promo_deleter.php">Remove items from promo banner</a><br>
-<a href="promo_viewer.php">Edit items in promo banner</a><br>
-</p>
-<h2>Navigation manager</h2>
-<p>Items need to be checked that the URL exists</p>
-<p>
-<a href="navigation_add.php">Add</a><br>
-<a href="navigation_viewer.php">Edit / Remove</a><br>
-</p>
-<h2>Article mangement</h2>
-<p>Options for handling the article pages.</p>
-<p>
-<a href="article_add.php">Add</a><br>
-<a href="article_deleter.php">Remove</a><br>
-<a href="article_viewer.php">Manage</a> - For editing, publishing, unpublishing, and previews<br></p>
-<h2>Gallery Management</h2>
-<p>Here we can add new albums, edit the albums etc. <a href="gallery.php">View Albums</a></p><p>
-<a href="album_add.php">Add New Album</a><br>
-<a href="album_change.php">Modify an Album</a> - title, public, etc<br>
-<a href="album_insert.php">Insert images into an album</a> - If an image has been uploaded, but placed in the wrong album<br></p>
-<h2>Image Management</h2>
-<p>Here we can add new images, Change some of the details about the images etc. <!--<a href="img_view.php">View Images</a>--></p><p>
-<a href="img_add.php">Add New Image</a><br>
-<a href="img_change.php">Change Image Details Permissions Etc.</a><br>
-</p>
+Also some description. Ideally should be a lighter weight than the content pages, less images etc. perhaps needing a separate style sheet?<br></p>
 
+<h2>Site Content</h2>
+<table><tr><td style="width:50%;"><h3>Article mangement</h3>
+<p>Options for handling the article pages.</p><p>
+* <a href="article_add.php">Add</a><br>
+* <a href="article_deleter.php">Remove</a><br>
+* <a href="article_viewer.php">Manage</a> - For editing, publishing, unpublishing, and previews<br></p></td>
+<td>
+<h3>Flash updates management</h2>
+<p>Options to add or remove a flash update</p><p>
+* <a href="flash_add.php">Add new Flash update</a><br>
+* <a href="flash_view.php">Manage existing Flash update</a><br>
+<i>To view the archive visit the public archive from the front page</i>
+</p></td></tr></table>
 
+<h2>Imagery Content</h2>
+<table><tr><td style="width:50%;"><h3>Gallery Management</h3>
+<p>Here we can add new albums, edit the albums etc.></p><p>
+* <a href="album_add.php">Add New Album</a><br>
+* <a href="album_change.php">Modify an Album</a><br>
+* <a href="album_insert.php">Link existing image into an album</a><br>
+* <a href="gallery.php">View all Albums</a> - Including unpublished</p></td>
 
+<td><h3>Image Management</h3>
+<p>Here we can add new images, Change some of the details about the images etc.</p><p>
+* <a href="img_add.php">Add New Image</a><br>
+* <a href="img_change.php">Change Image Details Permissions Etc.</a><br>
+</p></td></tr></table>
+
+<h2>Site Structure</h2>
+<table><tr><td style="width:50%;"><h3>Promo banner manager</h3>
+<p>Image and links for the promotion banner at the top of the home page.</p><p>
+* <a href="promo_adder.php">Add item to promo banner</a><br>
+* <a href="promo_deleter.php">Remove items from promo banner</a><br>
+* <a href="promo_viewer.php">Edit items in promo banner</a><br></p></td>
+
+<td><h3>Navigation manager</h3>
+<p>Items need to be checked that the URL exists</p><p>
+* <a href="navigation_add.php">Add</a><br>
+* <a href="navigation_viewer.php">Edit / Remove</a><br></p></td></tr></table>
 </div>
-</BODY>
-</HTML>
+</body>
+</html>
