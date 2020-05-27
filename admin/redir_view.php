248730b0795b6440007a16b35991489fefc8b985
a-mccluskey <46331308+a-mccluskey@users.noreply.github.com>
2020-05-27 22:26:00 +0100

    view redirection list

---
+++ admin/redir_view.php
@@ -0,0 +1,46 @@
+<?php 
+  include('session.php');
+  $isFrontPage = False;
+
+  //Do we want the user/article naviagtion structure
+  $isUserPage = False;
+ 
+  //Do we want the Admin naviagtion structure
+  $isAdminPage = True;
+
+  $page_title = "Admin: View The redirection Links";
+  include '../template/index.php';
+  echo "<h1>".$page_title."</h1>\n";
+  echo "<p>List of all the redirection links and their hit count\n</p><p>";
+
+  $redir_guid = "RedirGUID";
+  $redir_URL = "RedirURL";
+  $redir_views = "RedirViews";
+
+  require_once('../config.php');
+
+  $link=connectToAWDB();
+  $result= dbQuery("SELECT * FROM sc_redirect", $link);
+
+  $redir_data = array();
+  $i=0;
+  while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+  {
+  $redir_data[$i][$redir_guid] = $row['redirect_guid'];
+  $redir_data[$i][$redir_URL] = $row['redirect_URL'];
+  $redir_data[$i][$redir_views] = $row['redirect_hits'];
+  $i++;
+ }
+ disconnectAWDB($link);
+ $i--;
+ echo "<table border=1><tr><td>Code</td><td>Redirect URL</td><td>Copy this for the link</td><td>Hits</td></tr>\n\n";
+
+ //loop the list of pages
+ for ($j=0; $j<=$i; $j++) {
+    echo "<tr><td>".$redir_data[$j][$redir_guid]."</td>";
+    echo "<td><a href=\"article_edit.php?id=".$redir_data[$j][$redir_URL].'">' .$redir_data[$j][$redir_URL]."</a></td>";
+    echo "<td><code>".$MAIN_DOMAIN."r.php?loc=".$redir_data[$j][$redir_guid]."</code></td>";
+    echo "<td>".$redir_data[$j][$redir_views]." Hits</td></tr>\n\n";
+   }
+ echo "\n </table>";
+?></p></div></body></html>
