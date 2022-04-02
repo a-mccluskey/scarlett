69333d89d31dd3463ec790bbd9653184de5b2f17
Adam McCluskey <aMcCluskey@calrom.com>
2022-04-02 17:51:23 +0100

    Add initial unit tests project

---
+++ ScarNet.Tests/ModelTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScarNet.Models;
+
+namespace ScarNet.Tests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
