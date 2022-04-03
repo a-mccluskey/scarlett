463b140dbc2007c115cc9490993680aa9381b3bc
Adam McCluskey <aMcCluskey@calrom.com>
2022-04-03 20:35:03 +0100

    First test

--- ScarNet.Tests/ModelTests.cs
+++ ScarNet.Tests/ModelTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScarNet.Models;
+using System;
 
 namespace ScarNet.Tests
 {
@@ -7,8 +8,24 @@ namespace ScarNet.Tests
     public class ModelTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ArticleTest1()
         {
+            //// Setup
+            string title = "Test Title";
+            string text = "Article Text";
+            DateTime updated = new DateTime();
+            DateTime created = new DateTime();
+            string preview = "Preview Text";
+            int id = 0;
+
+            //// Act
+            var sut = new Article(title, text, updated, created, preview, id);
+
+            ////Assert
+            Assert.AreEqual(sut.Title, title);
+            Assert.AreEqual(sut.Text, text);
+            Assert.AreEqual(sut.Updated, updated);
+            Assert.AreEqual(sut.Created, created);
         }
     }
 }
